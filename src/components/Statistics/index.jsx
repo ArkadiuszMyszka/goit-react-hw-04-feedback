67599c48d51b54c3c05213b13a541048f6637e9b
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-08-18 04:14:06 +0200

    Statistics done

--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -1,28 +1,23 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
-export class Statistics extends Component {
-  static propTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    total: PropTypes.number,
-    positivePercentage: PropTypes.string,
-  };
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {total}</li>
+      <li>Positive feedback: {positivePercentage}</li>
+    </ul>
+  );
+};
 
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
-
-    return (
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive feedback: {positivePercentage}</li>
-      </ul>
-    );
-  }
-}
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.string,
+};
 
 export default Statistics;
